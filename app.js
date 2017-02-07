f516e35142f8e226ad54936a3a96d284df672d97
Esteban Padilla <ep@estebanpadilla.com>
2017-02-06 19:10:44 -0600

    Save

--- app.js
+++ app.js
@@ -6,130 +6,175 @@ function init() {
   canvas.height = window.innerHeight;
   document.body.appendChild(canvas);
   let context = canvas.getContext('2d');
+  let xpos = 300;
+  let ypos = 250;
+  let clockWidth = 188;
+
+  renderClock();
   var theDate = new Date();
   var sec = theDate.getSeconds();
+  setSeconds(sec);
 
+  let intervalId = setInterval(clock, 1000);
 
-  //dibujar reloj
-  //horilla negra
-  context.beginPath();
-  context.strokeStyle = 'black';
-  context.fillStyle = 'black';
-  context.arc(300, 280, 188, 0, Math.PI * 2, true);
-  context.fill();
-  context.stroke();
-  context.closePath();
-  //La cara
-  context.beginPath();
-  context.strokeStyle = 'grey';
-  context.fillStyle = 'grey';
-  context.arc(300, 280, 170, 0, Math.PI * 2, true);
-  context.fill();
-  context.stroke();
-  context.closePath();
-  //ojos
-  context.beginPath();
-  context.strokeStyle = 'white';
-  context.fillStyle = 'white';
-  context.arc(220, 220, 50, 0, Math.PI * 2, true);
-  context.fill();
-  context.stroke();
-  context.closePath();
+  function clock() {
+    sec++;
+    if (sec > 59) {
+      sec = 0;
+    }
 
-  context.beginPath();
-  context.strokeStyle = 'white';
-  context.fillStyle = 'white';
-  context.arc(380, 220, 50, 0, Math.PI * 2, true);
-  context.fill();
-  context.stroke();
-  context.closePath();
+    console.log('clock');
+    // hour(theDate);
+    // minute(theDate);
+    renderClock();
+    setSeconds(sec);
+  }
 
-  context.beginPath();
-  context.strokeStyle = 'black';
-  context.fillStyle = 'black';
-  context.arc(240, 220, 25, 0, Math.PI * 2, true);
-  context.fill();
-  context.stroke();
-  context.closePath();
+  function renderClock() {
+    //dibujar reloj
+    //horilla negra
+    context.beginPath();
+    context.strokeStyle = 'black';
+    context.fillStyle = 'black';
+    context.arc(xpos, ypos, clockWidth, 0, Math.PI * 2, true);
+    context.fill();
+    context.stroke();
+    context.closePath();
 
-  context.beginPath();
-  context.strokeStyle = 'black';
-  context.fillStyle = 'black';
-  context.arc(360, 220, 25, 0, Math.PI * 2, true);
-  context.fill();
-  context.stroke();
-  context.closePath();
-  //nariz
-  context.beginPath();
-  context.strokeStyle = 'black';
-  context.fillStyle = 'black';
-  context.ellipse(300, 280, 35, 8, 0, 0, Math.PI * 2, true);
-  context.fill();
-  context.stroke();
-  context.closePath();
+    //La cara
+    context.beginPath();
+    context.strokeStyle = 'grey';
+    context.fillStyle = 'grey';
+    context.arc(xpos, ypos, clockWidth - 20, 0, Math.PI * 2, true);
+    context.fill();
+    context.stroke();
+    context.closePath();
 
-  context.beginPath();
-  context.strokeStyle = 'black';
-  context.fillStyle = 'black';
-  context.arc(300, 290, 10, 0, Math.PI * 2, true);
-  context.fill();
-  context.stroke();
-  context.closePath();
-  //panza
-  context.beginPath();
-  context.fillStyle = '#EFE0B1';
-  context.ellipse(300, 390, 110, 60, 0, 0, Math.PI * 2, true);
-  context.fill();
-  context.closePath();
+    //ojos
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.fillStyle = 'white';
+    context.arc(xpos - 80, ypos - 80, 50, 0, Math.PI * 2, true);
+    context.fill();
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.fillStyle = 'white';
+    context.arc(xpos + 80, ypos - 80, 50, 0, Math.PI * 2, true);
+    context.fill();
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.strokeStyle = 'black';
+    context.fillStyle = 'black';
+    context.arc(xpos - 60, ypos - 80, 25, 0, Math.PI * 2, true);
+    context.fill();
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.strokeStyle = 'black';
+    context.fillStyle = 'black';
+    context.arc(xpos + 60, ypos - 80, 25, 0, Math.PI * 2, true);
+    context.fill();
+    context.stroke();
+    context.closePath();
+
+    //nariz
+    context.beginPath();
+    context.strokeStyle = 'black';
+    context.fillStyle = 'black';
+    context.ellipse(xpos, ypos - 20, 35, 8, 0, 0, Math.PI * 2, true);
+    context.fill();
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.strokeStyle = 'black';
+    context.fillStyle = 'black';
+    context.arc(xpos, ypos - 10, 10, 0, Math.PI * 2, true);
+    context.fill();
+    context.stroke();
+    context.closePath();
+    //panza
+    context.beginPath();
+    context.fillStyle = '#EFE0B1';
+    context.ellipse(xpos, ypos + 100, 110, 60, 0, 0, Math.PI * 2, true);
+    context.fill();
+    context.closePath();
+
+    renderNumbers();
+  }
+
   //numeros
-  function numbers() {
-    let ang;
+  function renderNumbers() {
+
+    let angle = 14.75;
+    let angleRatio = 360 / 12;
     let num;
+
     //tamaño de los nnumeros
     context.font = 188 * 0.08 + "px arial";
     context.textBaseline = "middle";
     context.textAlign = "center";
+
     //Creación de los numeros, se empieza con 1, ya que es del 1 al 12 y asi no se dibuja el 0 en vez del 12
+    let hip = 80;
+    let x = xpos;
+    let y = ypos - 155;
+
     for (num = 1; num < 13; num++) {
-      ang = num * Math.PI / 6;
-      context.rotate(ang);
-      context.translate(0, -188 * 0.85);
-      context.rotate(-ang);
-      context.fillText(num.toString(), 300, 280);
-      context.rotate(ang);
-      context.translate(0, 188 * 0.85);
-      context.rotate(-ang);
+
+
+      x += Math.cos(degreesToRadians(angle)) * hip;
+      y += Math.sin(degreesToRadians(angle)) * hip;
+
+
+      // context.translate(xpos, ypos);
+      // context.rotate(ang);
+
+      // context.rotate(ang);
+      // context.translate(0, -188 * 0.85);
+      // context.rotate(-ang);
+
+      context.fillText(num.toString(), x, y);
+
+
+      angle += angleRatio;
+      // context.rotate(ang);
+      // context.translate(0, 188 * 0.85);
+      // context.rotate(-ang);
+      // context.resetTransform();
+
     }
-    context.save();
+    // context.save();
   }
-  numbers();
-
-  function clock() {
-
-    // hour(theDate);
-    // minute(theDate);
-    seconds(theDate);
-    console.log(sec);
-
-  }
-  setInterval(clock(), 1000);
-
 
   //radianes
   function degreesToRadians(degrees) {
-    return  degrees * Math.PI / 180;
+    return degrees * Math.PI / 180;
   }
   //segundos
-  function seconds(theDate) {
+  function setSeconds(sec) {
+
+    console.log(sec);
 
     context.save();
+    context.translate(xpos, ypos);
+    context.rotate(degreesToRadians(sec * 6));
     context.beginPath();
     context.strokeStyle = 'red';
-    context.moveTo(300, 288);
-    context.lineTo(300, 130);
-    context.rotate(degreesToRadians(seconds * 13));
+    // context.moveTo(300, 288);
+    // context.lineTo(300, 130);
+    context.lineWidth = 3;
+    context.moveTo(0, 0);
+    context.lineTo(0, -170);
     context.stroke();
     context.closePath();
+    context.resetTransform();
     context.restore();
   }
   //minutos
@@ -162,3 +207,4 @@ function init() {
   //   context.restore();
   // }
 }
+
